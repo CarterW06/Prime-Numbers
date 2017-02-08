09066970587f6336fc853b2861b8591f997ba26e
Carter <benanddana92@gmail.com>
2017-02-08 15:55:08 -0800

    Added isPrime method, wrote first draft for isPrime

--- src/PrimeCheck.java
+++ src/PrimeCheck.java
@@ -1,4 +1,13 @@
 
 public class PrimeCheck {
+	// both methods return 0 if composite, 1 if prime
 	
+	public int isPrime(int input) {
+		for(int i = 2; i < input; i++) {
+			if (input % i == 0) {
+				return 0;
+			}
+		}
+		return 1;
+	}
 }
