0e681a91570a914df763eed364afed00ee9de769
Carter <benanddana92@gmail.com>
2017-02-08 16:22:32 -0800

    Working isPrime method, added long input method that is defective

--- src/PrimeCheck.java
+++ src/PrimeCheck.java
@@ -2,7 +2,17 @@
 public class PrimeCheck {
 	// both methods return 0 if composite, 1 if prime
 	
-	public int isPrime(int input) {
+	public static int isPrime(int input) {
+		for(int i = 2; i < input; i++) {
+			if (input % i == 0) {
+				return 0;
+			}
+		}
+		return 1;
+	}
+	
+	//TODO make this method work
+	public static int isPrime(long input) {
 		for(int i = 2; i < input; i++) {
 			if (input % i == 0) {
 				return 0;
---
+++ src/PrimeCheckTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+// TEMPORARY - DELETE WHEN FINISHED!!!
+public class PrimeCheckTest {
+
+	@Test
+	public void test() {
+		// No current tests.
+	}
+
+}
